5f21a42987abbe0dbde8dd16fb3bb28240871538
Alexander Fatyukhin <virikidorhom@gmail.com>
2022-09-23 13:45:08 +0300

    new design

--- resources/views/shop/shop_master_new.blade.php
+++ resources/views/shop/shop_master_new.blade.php
@@ -34,67 +34,9 @@
 <div class="wrapper">
     <div class="content">
         <div class="modal"></div>
-        <header class="header">
-            <div class="container">
-                <div class="header__body">
-                    <div class="header__mobile-btn">
-                        <button class="menu-btn">
-                            <span></span>
-                        </button>
-                    </div>
-                    <div class="header__login">
-                        <a href="#"><img src="/assets/images/icons/bag.svg" alt=""></a>
-                        <a href="#"><img src="/assets/images/icons/user.svg" alt=""></a>
-                    </div>
-                    <div class="header__title">
-                        <a href="#">
-                            <img src="/assets/images/logo.png" alt="">
-                        </a>
-                    </div>
-                    <nav class="header__nav">
-                        <ul class="header__row header-top">
-                            <li>
-                                <a href="#">
-                                    О нас
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Доставка и Оплата
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Отзывы
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Контакты
-                                </a>
-                            </li>
-                        </ul>
-{{--                        <ul class="header__row header-bottom">--}}
-{{--                            <li>--}}
-{{--                                <a class="active" href="#">--}}
-{{--                                    Все--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                            <li>--}}
-{{--                                <a href="#">--}}
-{{--                                    В наличии--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                            <li>--}}
-{{--                                <a href="#">--}}
-{{--                                    Под заказ--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                        </ul>--}}
-                    </nav>
-                </div>
-            </div>
-        </header>
+
+        @include('shop.new.ru.header')
+
         <main class="main">
             <div class="container">
                 <div class="main__wrap">
@@ -142,6 +84,7 @@
                             </li>
                         </ul>
                     </div>
+
                     @section('content')
 
                     @show
