65fd88bafbbc599ae615f41aec00ce41022a2abb
Alex-Dell <virikidorhom@gmail.com>
2022-12-14 03:01:50 +0200

    fix filters all

--- resources/views/shop/new/layouts/category_products.blade.php
+++ resources/views/shop/new/layouts/category_products.blade.php
@@ -1,9 +1,9 @@
 
 @if($products)
     <div class="card__items">
-    @foreach($products as $product)
-        @include('shop.new.layouts.components.product_item')
-    @endforeach
+        @foreach($products as $product)
+            @include('shop.new.layouts.components.product_item')
+        @endforeach
 
         @if(isset($products2) && $products2)
             @foreach($products2 as $product)
