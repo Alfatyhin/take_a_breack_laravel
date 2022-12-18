0c156023a8a5bd79312366b75173a8287dda8bcc
Alex-Dell <virikidorhom@gmail.com>
2022-12-18 14:07:43 +0200

    styles mobail menu

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -28,7 +28,7 @@ use function PHPUnit\Framework\matches;
 class ShopController extends Controller
 {
 
-    private $v = '2.2.9';
+    private $v = '2.2.7';
 
     public function err404(Request $request, $lang = 'en')
     {
