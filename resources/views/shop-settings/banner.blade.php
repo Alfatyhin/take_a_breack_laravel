fbc659bdd47604403d4415af9da0b111db96bd00
Alex-Dell <virikidorhom@gmail.com>
2022-12-31 11:39:01 +0200

    popapp bekend add checkbox banner

--- resources/views/shop-settings/banner.blade.php
+++ resources/views/shop-settings/banner.blade.php
@@ -4,7 +4,8 @@
 
 
 @section('head')
-
+{{--    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>--}}
+{{--    <script>tinymce.init({ selector:'textarea.redact' });</script>--}}
 @stop
 
 @section('sidebar')
@@ -17,8 +18,12 @@
    <form action="{{ route('banner') }}" method="POST">
        @csrf
        <div>
-           <b>popap </b>
-           режим попап: <br>
+           <b>banner </b>
+           <label>
+               <input type="checkbox" name="banner[banner]" value="1" @isset($banner['banner']) checked @endisset > вкл
+           </label>
+           <br>
+           <b>режим попап: </b>
            <label>
                <input type="checkbox" name="banner[popapp]" value="1" @isset($banner['popapp']) checked @endisset > вкл
            </label>
@@ -27,21 +32,21 @@
        <div>
            <b>Язык: en </b>
            Текст: <br>
-           <textarea name="banner[en]">{{ $banner['en'] }}</textarea>
+           <textarea class="redact" name="banner[en]">{{ $banner['en'] }}</textarea>
        </div>
        <hr>
        <div>
            <b>Язык: ru </b>
 
            Текст: <br>
-           <textarea name="banner[ru]">{{ $banner['ru'] }}</textarea>
+           <textarea class="redact" name="banner[ru]">{{ $banner['ru'] }}</textarea>
        </div>
        <hr>
        <div>
            <b>Язык: he </b>
 
            Текст: <br>
-           <textarea name="banner[he]">{{ $banner['he'] }}</textarea>
+           <textarea class="redact" name="banner[he]">{{ $banner['he'] }}</textarea>
        </div>
        <input type="submit" >
    </form>
