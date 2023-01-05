dbf0745a647e1431745679890f8d4aedcb18cb99
Alex-Dell <virikidorhom@gmail.com>
2023-01-05 10:43:45 +0200

    change view port

--- resources/views/shop/new/shop_master.blade.php
+++ resources/views/shop/new/shop_master.blade.php
@@ -10,7 +10,8 @@
     </title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1.0">--}}
+    <meta name="viewport" content="width=device-width, user-scalable=no">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 
     <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
