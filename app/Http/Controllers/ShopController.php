d34fa9ce97ffb64e701f362caeeb76e3eb0e61a8
Alex-Dell <virikidorhom@gmail.com>
2023-01-09 02:07:41 +0200

    view

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -28,7 +28,7 @@ use function PHPUnit\Framework\matches;
 class ShopController extends Controller
 {
 
-    private $v = '2.4.5';
+    private $v = '2.4.6';
 
     public function err404(Request $request, $lang = 'en')
     {
