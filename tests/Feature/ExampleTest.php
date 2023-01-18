561df830f2e6e33e5f916562ef39331243d39cbe
Alex-Dell <virikidorhom@gmail.com>
2023-01-18 19:06:35 +0200

    fix test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,9 +13,45 @@ class ExampleTest extends TestCase
      * @return void
      */
     /** @test */
-    public function test_example()
+    public function test_index()
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    public function test_index_ru()
+    {
+        $response = $this->get('ru');
+
+        $response->assertStatus(200);
+    }
+    public function test_index_he()
+    {
+        $response = $this->get('he');
+
+        $response->assertStatus(200);
+    }
+    public function test_getIcreditUrl()
+    {
+        $response = $this->post('orders/icredit');
+
+        $response->assertStatus(200);
+    }
+    public function test_amoWebhok()
+    {
+        $response = $this->post('api/amocrm/amowebhok');
+
+        $response->assertStatus(200);
+    }
+    public function test_ruInstock()
+    {
+        $response = $this->get('/ru/in_stock');
+
+        $response->assertStatus(200);
+    }
+    public function test_crm()
+    {
+        $response = $this->post('crm');
 
         $response->assertStatus(200);
     }
