e1c3b647c0309bff8e8104639562e5cb705dd386
Alex-Dell <virikidorhom@gmail.com>
2023-03-17 10:03:51 +0200

    fix status order

--- app/Http/Middleware/ShopThanks.php
+++ app/Http/Middleware/ShopThanks.php
@@ -72,6 +72,7 @@ class ShopThanks
 
                 if (env('APP_NAME') != "Take a Break Server") {
                     try {
+
                         $OrderService->createOrderToAmocrm($order_id, '43924885');
                         WebhookLog::addLog('OrderThanks After create AMO Lead', "$order_id");
                     } catch (Exception $e) {
