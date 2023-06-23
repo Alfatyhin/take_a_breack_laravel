e54a242303f14f713993794e4eebc81a5c17c302
Alex-Dell <virikidorhom@gmail.com>
2023-06-23 08:58:43 +0300

    google schets

---
+++ app/Services/GoogleSheets.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Services;
+
+
+use Google\Service\Sheets;
+
+class GoogleSheets extends GClientService
+{
+
+    private $service;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $client = $this->getClient();
+        $client->addScope(Sheets::DRIVE);
+        $client->setSubject('docs-993@centered-router-381409.iam.gserviceaccount.com');
+
+        $service = new Sheets($client);
+        $this->service = $service;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    /**
+     * @param mixed $service
+     */
+    public function setService($service): void
+    {
+        $this->service = $service;
+    }
+
+
+}
